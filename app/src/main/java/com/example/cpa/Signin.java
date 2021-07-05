a00a5eefe4827f6329e11810979843322dbc91cb
Owais <oveskhan.180@gmail.com>
2021-07-06 01:16:22 +0500

    3rd commit

--- app/src/main/java/com/example/cpa/Signin.java
+++ app/src/main/java/com/example/cpa/Signin.java
@@ -1,7 +1,11 @@
 package com.example.cpa;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,10 +18,52 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Signin extends Activity {
 
+    EditText emaillogin;
+    EditText passwordlogin;
+    Button loginbtn;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_login);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emaillogin.findViewById(R.id.emaillogin);
+        passwordlogin.findViewById(R.id.passwordlogin);
+        loginbtn=findViewById(R.id.loginbtn);
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.createUserWithEmailAndPassword(emaillogin.getText().toString().trim(),passwordlogin.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Signin.this,"login Successfull",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Signin.class));
+
+
+                        }else{
+
+                            Toast.makeText(Signin.this,"Error!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            }
+        });
+
+
 
 
     }
