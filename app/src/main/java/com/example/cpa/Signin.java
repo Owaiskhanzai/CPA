2da3c8cdbaf44f8c49eaf5d8037bcbd95f305655
Owais <letsfiregoo@gmail.com>
2021-07-30 21:10:20 +0500

    dashboard seperated

--- app/src/main/java/com/example/cpa/Signin.java
+++ app/src/main/java/com/example/cpa/Signin.java
@@ -12,10 +12,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Signin extends Activity {
 
@@ -24,15 +29,16 @@ public class Signin extends Activity {
     Button loginbtn;
     TextView registerhere;
     FirebaseAuth mAuth;
-
-
+    private FirebaseFirestore fstore;
+     Object Buyer="Buyer";
+     Object Seller="Seller";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        fstore=FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
         emaillogin=findViewById(R.id.emaillogin);
@@ -52,6 +58,10 @@ public class Signin extends Activity {
             @Override
             public void onClick(View v) {
 
+
+
+
+
                 loginUser();
 
             }
@@ -60,29 +70,131 @@ public class Signin extends Activity {
 
 
     }
-    public void loginUser(){
+    public void loginUser() {
 
-mAuth.signInWithEmailAndPassword(emaillogin.getText().toString().trim(),passwordlogin.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-    @Override
-    public void onComplete(@NonNull Task<AuthResult> task) {
+        mAuth.signInWithEmailAndPassword(emaillogin.getText().toString().trim(), passwordlogin.getText().toString().trim()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
 
-        if(task.isSuccessful()){
+                checkuserAccesslevel(mAuth.getCurrentUser().getUid());
+                Toast.makeText(Signin.this, "Login Successfully", Toast.LENGTH_SHORT).show();
 
-            Toast.makeText(Signin.this,"Login Successfully",Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(getApplicationContext(), DashActivity.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
 
-        }
-        else{
+                Toast.makeText(Signin.this, "Error!" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-            Toast.makeText(Signin.this,"Error!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-        }
+    }//loginuser
 
+
+
+
+    private void checkuserAccesslevel(String uid) {
+
+        DocumentReference df=fstore.collection("user").document(uid);
+
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                Log.d("TAG","Success"+documentSnapshot.getData());
+
+                if(documentSnapshot.getString("usertype").equals(Seller) ){
+
+                    startActivity(new Intent(getApplicationContext(), DashActivity.class));
+                    finish();
+                }
+                if(documentSnapshot.getString("usertype").equals(Buyer)){
+
+                    startActivity(new Intent(getApplicationContext(), userActivity.class));
+                    finish();
+                }
+
+            }
+        });
 
     }
-});
+
+
+    protected void onStart() {
+
+
+        super.onStart();
+
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+
+        DocumentReference df=FirebaseFirestore.getInstance().collection("user").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                if(documentSnapshot.getString("usertype").equals(Seller) ){
+
+                    startActivity(new Intent(getApplicationContext(), DashActivity.class));
+                    finish();
+                }
+                if(documentSnapshot.getString("usertype").equals(Buyer)){
+
+                    startActivity(new Intent(getApplicationContext(), userActivity.class));
+                    finish();
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Signin.class));
+                finish();
+
+            }
+        });
+
+
+        }
+    }
+
+    protected void onDestroy() {
+
+
+        super.onDestroy();
+
+        
 
     }
 
 }
 
 //emaillogin.getText().toString().trim(),passwordlogin.getText().toString().trim()
+
+
+
+//
+//
+//mAuth.signInWithEmailAndPassword(emaillogin.getText().toString().trim(),passwordlogin.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//    @Override
+//    public void onComplete(@NonNull Task<AuthResult> task) {
+//
+//        if(task.isSuccessful()){
+//
+//            Toast.makeText(Signin.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+//            checkuserAccesslevel(mAuth.getCurrentUser().getUid());
+//
+//
+//
+//        }
+//        else{
+//
+//            Toast.makeText(Signin.this,"Error!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+//        }
+//
+//
+//    }
+//});
